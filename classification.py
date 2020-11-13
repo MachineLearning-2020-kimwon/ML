fab4f86be051c356f21585ea7ee8b20f606aa3f1
yerin1106 <51941176+yerin1106@users.noreply.github.com>
2020-11-13 15:01:45 +0900

    Create classification.py

---
+++ classification.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Load the data
+df = pd.read_csv('C:/Users/samsung/Desktop/hotel_bookings.csv')
+df = df.drop('agent', axis=1)
+df = df.drop('company', axis=1)
+
+# dirty data preprocessing
+
+
+# encoding
+encoder = LabelEncoder()
+for cols in df.columns:
+    df[cols] = encoder.fit_transform(df[cols])
+
+#scaling
+MinMax_scaler = MinMaxScaler()
+Standard_scaler = StandardScaler()
